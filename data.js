373a8fb6dcbe84f0dbb16f4ada409f757e19871a
barneyhill <barneyhill1@gmail.com>
2018-10-06 18:08:21 +0100

    Added dependencies

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 let GeoTIFF = require('geotiff')
 let getDepth = function(lat, long){
-  let tiff = GeoTIFF.fromFile('/media/chrx/SSD/srtm_10_01.tif');
+  const tiff = GeoTIFF.fromFile('/media/chrx/SSD/srtm_10_01.tif');
   const image = tiff.getImage();
   const width = image.getWidth();
   return width;
 
-}
+};
 module.exports.getDepth = getDepth;
