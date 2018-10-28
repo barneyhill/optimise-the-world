e6d674a5e909d06b0a59c706e9423e6a4e815be2
barneyhill <barneyhill1@gmail.com>
2018-10-28 15:47:06 +0000

    - Added Query parameters - Removed unnecessary passing of global variable into getDepth()

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ const util = require('util');
 hgt.TileSet.prototype.getElevationAsync = util.promisify(hgt.TileSet.prototype.getElevation);
 
 var tileset = new hgt.TileSet('/media/chrx/SSD1/data/');
-var getDepth = async function(tileset, lat, long){
+var getDepth = async function(lat, long){
 	var elevation = await tileset.getElevationAsync([lat, long]);
 	return elevation;
 }
 
-var recursive_gradient_descent = async function(tileset, velocity, J_history, coords, alpha, gamma, i){
+var recursive_gradient_descent = async function(velocity, J_history, coords, alpha, gamma, i){
 	i-=1;
 	if (await i <= 0){return J_history};
 
-	var cost = await getDepth(tileset, coords[0], coords[1]);
+	var cost = await getDepth(coords[0], coords[1]);
 	await J_history.push([cost, coords[0], coords[1]]);
 	if (cost <= 0){return J_history};
 
-	var elev1 = getDepth(tileset, coords[0] + 0.001, coords[1]);
-	var elev2 = getDepth(tileset, coords[0] - 0.001, coords[1]);
-	var elev3 = getDepth(tileset, coords[0], coords[1] + 0.001);
-	var elev4 = getDepth(tileset, coords[0], coords[1] - 0.001);
+	var elev1 = getDepth(coords[0] + 0.001, coords[1]);
+	var elev2 = getDepth(coords[0] - 0.001, coords[1]);
+	var elev3 = getDepth(coords[0], coords[1] + 0.001);
+	var elev4 = getDepth(coords[0], coords[1] - 0.001);
 
 	var lat_slope = await elev1 / await elev2 - 1;
 	var lon_slope = await elev3 / await elev4 - 1;
@@ -38,15 +38,16 @@ var recursive_gradient_descent = async function(tileset, velocity, J_history, co
 
 	console.log(coords[0], coords[1], i);
 
-	return recursive_gradient_descent(tileset, velocity, J_history, coords, alpha, gamma, i)
+	return recursive_gradient_descent(velocity, J_history, coords, alpha, gamma, i)
 }
 
 router.get('/', async ctx => {
 	ctx.body = JSON.stringify(await getDepth(tileset, 53.2734, -7.7783))
 });
 
-router.get('/test', async ctx => {
-	var J_history = await recursive_gradient_descent(tileset, [0,0], [], [53.2734, -7.7783], 0.01, 0.99, 1000);
+router.get('/gradient_descent', async ctx => {
+	console.log([ctx.query.lat, ctx.query.long], ctx.query.alpha, ctx.query.gamma, ctx.query.i)
+	var J_history = await recursive_gradient_descent([0,0], [], [parseFloat(ctx.query.lat), parseFloat(ctx.query.long)], parseFloat(ctx.query.alpha), parseFloat(ctx.query.gamma), parseInt(ctx.query.i));
 	ctx.body = await JSON.stringify(J_history);
 });
 
