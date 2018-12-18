0e1ab07c1f666c8a7c1fa1f8f9adb8aa76407dbd
barneyhill <barneyhill1@gmail.com>
2018-12-18 13:01:24 +0000

    Fixed a couple of bugs, final versions of momentum and classical gradient descent

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const hgt = require('node-hgt');
-const Koa = require('koa');
-const path = require('path')
+const hgt = require('node-hgt'); //node-hgt handles dataset requests
+const Koa = require('koa'); //koa is my chosen web framework
+const path = require('path');
 
-var app = new Koa();
+var app = new Koa(); //Initalises Koa object
 
 const serve = require('koa-static')
 
@@ -12,12 +12,14 @@ const router = new KoaRouter
 const perf_hooks = require('perf_hooks')
 const util = require('util');
 
-hgt.TileSet.prototype.getElevationAsync = util.promisify(hgt.TileSet.prototype.getElevation);
-var tileset = new hgt.TileSet('/media/chrx/SSD1/data/');
+hgt.TileSet.prototype.getElevationAsync = util.promisify(hgt.TileSet.prototype.getElevation); // Converts "getELevation" file into an asynchronous function
 
-var getDepth = async function(lat, long){
+var tileset = new hgt.TileSet('/media/chrx/SSD3/data/'); // Creates a cache of all hgt files found on the SSD
+
+// getDepth returns an elevation given two coordinates
+var getDepth = async function(lat, lng){
 	try{
-		var elevation = await tileset.getElevationAsync([lat, long]);
+		var elevation = await tileset.getElevationAsync([lat, lng]);
 	} catch (error){
 		console.log("Invalid coordinate");
 		return -1;
@@ -26,15 +28,12 @@ var getDepth = async function(lat, long){
 }
 
 
-var gradient_descent = async function(J_history, precision, learning_rate, i){
+var gradient_descent = async function(history, precision, learning_rate, i){
 
 	i--;
-	if (i==0){return J_history};
+	if (await i<=0){return history};
 
-	var coords = J_history[J_history.length - 1].slice(1, 3);
-
-	var cost = await getDepth(coords[0], coords[1]);
-	if (cost == 0){return J_history};
+	var coords = history[history.length - 1].slice(1, 3);
 
 	var elev1 = getDepth(coords[0] + 0.001, coords[1]);
 	var elev2 = getDepth(coords[0] - 0.001, coords[1]);
@@ -42,70 +41,87 @@ var gradient_descent = async function(J_history, precision, learning_rate, i){
 	var elev4 = getDepth(coords[0], coords[1] - 0.001);
 
 	var lat_slope = await elev1 / await elev2 - 1;
-	var lon_slope = await elev3 / await elev4 - 1;
+	var lng_slope = await elev3 / await elev4 - 1;
 
-	console.log(i, await lat_slope, await lon_slope);
-
-	if (Math.abs(lat_slope)+Math.abs(lon_slope) < precision){return J_history};
+	if (Math.abs(lat_slope)+Math.abs(lng_slope) < precision){
+		console.log("Local minimum was reached!")
+		return history
+	};
 
 	coords[0] = coords[0] - learning_rate*lat_slope;
-	coords[1] = coords[1] - learning_rate*lon_slope;
-
-	await J_history.push([cost, coords[0], coords[1]]);
-
-	return gradient_descent(J_history, precision, learning_rate, i)
-}
-
-var momentum_gradient_descent = async function(velocity, J_history, coords, alpha, gamma, i){
-	i-=1;
-	if (await i <= 0){return J_history};
+	coords[1] = coords[1] - learning_rate*lng_slope;
 
 	var cost = await getDepth(coords[0], coords[1]);
-	await J_history.push([cost, coords[0], coords[1]]);
-	if (cost <= 0){return J_history};
 
-	var elev1 = getDepth(coords[0] + 0.001, coords[1]);
-	var elev2 = getDepth(coords[0] - 0.001, coords[1]);
-	var elev3 = getDepth(coords[0], coords[1] + 0.001);
-	var elev4 = getDepth(coords[0], coords[1] - 0.001);
+	if (cost <= 0){
+		await history.push([cost, coords[0], coords[1]]);
+		return history
+	};
 
-	console.log(await elev1, await elev2, await elev3, await elev4);
+	await history.push([cost, coords[0], coords[1]]);
 
-	var lat_slope = await elev1 / await elev2 - 1;
-	var lon_slope = await elev3 / await elev4 - 1;
+	console.log(cost, coords[0], coords[1]);
 
-	velocity[0] = gamma * velocity[0] + alpha * lat_slope;
-	velocity[1] = gamma * velocity[1] + alpha * lon_slope;
+	return gradient_descent(history, precision, learning_rate, i)
+}
+var momentum_gradient_descent = async function(velocity, history, precision, learning_rate, i){
+	i-=1;
+	if (await i <= 0){return history};
+
+	console.log(history[history.length - 1]);
+
+	var coords = history[history.length - 1].slice(1, 3);
+
+	var elev1 = await getDepth(coords[0] + 0.001, coords[1]);
+	var elev2 = await getDepth(coords[0] - 0.001, coords[1]);
+	var elev3 = await getDepth(coords[0], coords[1] + 0.001);
+	var elev4 = await getDepth(coords[0], coords[1] - 0.001);
+
+	var lat_slope = elev1 / elev2 - 1;
+	var lon_slope = elev3 / elev4 - 1;
+
+	if (Math.abs(lat_slope)+Math.abs(lon_slope) < precision){return history};
+
+	velocity[0] = learning_rate * velocity[0] + (1-learning_rate) * lat_slope;
+	velocity[1] = learning_rate * velocity[1] + (1-learning_rate) * lon_slope;
 
 	coords[0] = coords[0] - velocity[0];
 	coords[1] = coords[1] - velocity[1];
 
-	console.log(coords, lat_slope, lon_slope, velocity);
+	var cost = await getDepth(coords[0], coords[1]);
+	if (cost <= 0){
+		await history.push([cost, coords[0], coords[1]]);	
+		return history;
+	};
 
-	return momentum_gradient_descent(velocity, J_history, coords, alpha, gamma, i)
+	console.log(coords, lat_slope, lon_slope, velocity);
+	await history.push([cost, coords[0], coords[1]]);
+
+	return momentum_gradient_descent(velocity, history, precision, learning_rate, i);
 }
 
 
 app.use(serve(__dirname + '/public'));
 
 router.get('/', async ctx => {
-	ctx.body = JSON.stringify(await getDepth(ctx.query.lat, ctx.query.long));
+	ctx.body = JSON.stringify(await getDepth(ctx.query.lat, ctx.query.lng));
 });
-
-router.get('/gradient_descent_test', async ctx => {
-	var J_history = await gradient_descent([[-1, parseFloat(ctx.query.lat), parseFloat(ctx.query.long)]], ctx.query.precision, ctx.query.learning_rate, 1000);
-	for (item of J_history){console.log(item[2] + ", " + item[1])}
-	ctx.body = await JSON.stringify(J_history);
-});
-
 
 router.get('/gradient_descent', async ctx => {
-	console.log([ctx.query.lat, ctx.query.long], ctx.query.alpha, ctx.query.gamma, ctx.query.i)
-	var J_history = await momentum_gradient_descent([0,0], [], [parseFloat(ctx.query.lat), parseFloat(ctx.query.long)], parseFloat(ctx.query.alpha), parseFloat(ctx.query.gamma), parseInt(ctx.query.i));
-	for (item of J_history){console.log(item[2] + ", " + item[1])}
-	ctx.body = await JSON.stringify(J_history);
+	var history = await gradient_descent([[-1, parseFloat(ctx.query.lat), parseFloat(ctx.query.lng)]], parseFloat(ctx.query.precision), parseFloat(ctx.query.learning_rate), 100);
+	coordinates = [];
+	for (item of history){coordinates.push({lat: item[1], lng: item[2]})};
+	ctx.body = await JSON.stringify(coordinates);
+});
+
+
+router.get('/gradient_descent_momentum', async ctx => {
+	var history = await momentum_gradient_descent([0,0], [[-1, parseFloat(ctx.query.lat), parseFloat(ctx.query.lng)]], parseFloat(ctx.query.precision), parseFloat(ctx.query.learning_rate), 100);
+	coordinates = [];
+	for (item of history){coordinates.push({lat: item[1], lng: item[2]})};
+	ctx.body = await JSON.stringify(coordinates);
 });
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(8000);
+app.listen(2020);
