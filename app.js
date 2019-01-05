35dd1072aae8f390601d26cd97d62d07cd137074
barneyhill <barneyhill1@gmail.com>
2019-01-05 14:16:39 +0000

    Basic usability. Can select a point on the map

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ var gradient_descent = async function(history, precision, learning_rate, i){
 	var lat_slope = await elev1 / await elev2 - 1;
 	var lng_slope = await elev3 / await elev4 - 1;
 
-	if (Math.abs(lat_slope)+Math.abs(lng_slope) < precision){
+	if (Math.abs(await lat_slope)+Math.abs(await lng_slope) < precision){
 		console.log("Local minimum was reached!")
 		return history
 	};
@@ -54,6 +54,7 @@ var gradient_descent = async function(history, precision, learning_rate, i){
 	var cost = await getDepth(coords[0], coords[1]);
 
 	if (cost <= 0){
+		console.log("Sea level found!")
 		await history.push([cost, coords[0], coords[1]]);
 		return history
 	};
@@ -65,6 +66,9 @@ var gradient_descent = async function(history, precision, learning_rate, i){
 	return gradient_descent(history, precision, learning_rate, i)
 }
 var momentum_gradient_descent = async function(velocity, history, precision, learning_rate, i){
+	
+	console.log(velocity, learning_rate)
+
 	i-=1;
 	if (await i <= 0){return history};
 
@@ -77,24 +81,36 @@ var momentum_gradient_descent = async function(velocity, history, precision, lea
 	var elev3 = await getDepth(coords[0], coords[1] + 0.001);
 	var elev4 = await getDepth(coords[0], coords[1] - 0.001);
 
+	console.log(elev1,elev2,elev3,elev4)
+
 	var lat_slope = elev1 / elev2 - 1;
-	var lon_slope = elev3 / elev4 - 1;
+	var lng_slope = elev3 / elev4 - 1;
 
-	if (Math.abs(lat_slope)+Math.abs(lon_slope) < precision){return history};
+	if (Math.abs(await lat_slope)+Math.abs(await lng_slope) < precision){
+		console.log("Local minimum was reached!")
+		return history
+	};
 
-	velocity[0] = learning_rate * velocity[0] + (1-learning_rate) * lat_slope;
-	velocity[1] = learning_rate * velocity[1] + (1-learning_rate) * lon_slope;
+	velocity[0] = (1-learning_rate) * velocity[0] + learning_rate * await lat_slope;
+	velocity[1] = (1-learning_rate) * velocity[1] + learning_rate * await lng_slope;
+
+	console.log(velocity)
 
 	coords[0] = coords[0] - velocity[0];
 	coords[1] = coords[1] - velocity[1];
 
 	var cost = await getDepth(coords[0], coords[1]);
+
+	console.log(cost, coords)
+
 	if (cost <= 0){
+		console.log("Sea level found!")
+
+		console.log("Sea level found!")
 		await history.push([cost, coords[0], coords[1]]);	
 		return history;
 	};
 
-	console.log(coords, lat_slope, lon_slope, velocity);
 	await history.push([cost, coords[0], coords[1]]);
 
 	return momentum_gradient_descent(velocity, history, precision, learning_rate, i);
@@ -103,9 +119,9 @@ var momentum_gradient_descent = async function(velocity, history, precision, lea
 
 app.use(serve(__dirname + '/public'));
 
-router.get('/', async ctx => {
-	ctx.body = JSON.stringify(await getDepth(ctx.query.lat, ctx.query.lng));
-});
+//router.get('/depth', async ctx => {
+//	ctx.body = JSON.stringify(await getDepth(ctx.query.lat, ctx.query.lng));
+//});
 
 router.get('/gradient_descent', async ctx => {
 	var history = await gradient_descent([[-1, parseFloat(ctx.query.lat), parseFloat(ctx.query.lng)]], parseFloat(ctx.query.precision), parseFloat(ctx.query.learning_rate), 100);
@@ -114,8 +130,8 @@ router.get('/gradient_descent', async ctx => {
 	ctx.body = await JSON.stringify(coordinates);
 });
 
-
 router.get('/gradient_descent_momentum', async ctx => {
+	console.log("ran")
 	var history = await momentum_gradient_descent([0,0], [[-1, parseFloat(ctx.query.lat), parseFloat(ctx.query.lng)]], parseFloat(ctx.query.precision), parseFloat(ctx.query.learning_rate), 100);
 	coordinates = [];
 	for (item of history){coordinates.push({lat: item[1], lng: item[2]})};
@@ -124,4 +140,4 @@ router.get('/gradient_descent_momentum', async ctx => {
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(2020);
+app.listen(1234);
